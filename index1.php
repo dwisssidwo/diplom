343ef0d2fece6431d0eed33ed1cd086f6d4d4411
dwisssidwo <teos02@mail.ru>
2024-05-10 10:40:40 +0500

    Delete index1.php

--- index1.php
+++
@@ -1,6 +0,0 @@
-<form action="book_computer.php" method="POST">
-    <input type="text" name="computer_ID" placeholder="ID">
-    <input type="date" name="start_Date" placeholder="Дата заезда">
-    <input type="date" name="end_Date" placeholder="Дата выезда">
-    <button type="submit">Забронировать</button>
-</form>
